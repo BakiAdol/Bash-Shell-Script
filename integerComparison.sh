94e956fbe36a4cee3995cefc672ef9c08b9ff66b
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-07-20 11:08:05 +0600

    Update integerComparison.sh

--- integerComparison.sh
+++ integerComparison.sh
@@ -13,3 +13,22 @@ else
 		echo "You Older!"
 	fi
 fi
+
+: '
+Using parenthesis
+
+read age
+
+if (($age < 18))
+then
+	echo "You kids!"
+else
+	if (($age < 40))
+	then
+		echo "You Adult!"
+	else
+		echo "You Older!"
+	fi
+fi
+
+'
